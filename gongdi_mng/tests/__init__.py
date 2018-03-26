d81c4dd78b0de4e56f7d967a1d7eba9c516ba4c0
denjay <295077487@qq.com>
2018-03-26 15:14:56 +0800

    删除gongdi_mng

--- gongdi_mng/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-########################################
-# create by :cxh-PC
-# create time :2018-01-31 11:55:31.685293
-########################################
-import unittest
-def test():
-    """Run the unit tests."""
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
